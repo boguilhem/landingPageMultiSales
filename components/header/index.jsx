1a0a7867dfe8fab575a992b752f11372bc24a7ce
Bruno Guilhem <boguilhem@gmail.com>
2023-02-21 01:37:13 -0300

    fix: Image logo

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -7,23 +7,12 @@ const Header = () => {
   return (
     <div className={styles.container}>
       <div className={styles.logotipo}>
-        <Image src={Logo} />
+        <Image src={Logo} alt="Logo" />
       </div>
       <div className={styles.menu}>
         <Link href="/">Home</Link>
         <Link href="/">O que fazemos</Link>
         <Link href="/">Cases</Link>
-        {/* <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/">O que fazemos</Link>
-          </li>
-          <li>
-            <Link href="/">Cases</Link>
-          </li>
-        </ul> */}
       </div>
       <div className={styles.action}>
         <button className={styles.button}>Fale conosco</button>
